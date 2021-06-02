92515d58485da8708f0b6d5dd03fe74acec612e6
heleenvdongen <h.e.j.v.dongen@student.tue.nl>
2021-06-02 17:36:41 +0200

    attempt 1 to 3rd vis

--- Code/js/LineDiagram.js
+++ Code/js/LineDiagram.js
@@ -15,10 +15,21 @@ class LineDiagram {
    * @param {Array}  format            The visualization format.
    */
   constructor(json, format) {
-    console.log(json);
+    let data = JSON.parse(JSON.stringify(json));
+    console.log(data);
+    this.filters = [];
+
     this.jsonData = json;
+    console.log(json);
     this.data = this.filterData(json);
     this.format = format;
+
+    this.dateRange = this.jsonData["json[0]"];
+
+
+    this.width=600;
+    this.height=270;
+
 
     this.mapJSONData();
   }
@@ -27,7 +38,62 @@ class LineDiagram {
    * Parse JSON and map data.
    */
   mapJSONData() {
-    
+
+    this.x = d3.scaleTime().domain([0, width]);
+    console.log(this.x);
+    this.y0 = d3.scaleLinear().domain([height, 0]);
+    //var y1 = d3.scaleLinear().range([height, 0]);
+
+    this.xAxis = d3.axisBottom(this.x).ticks(5);
+
+    this.yAxisLeft = d3.axisLeft(this.y0).ticks(5);
+
+    //var yAxisRight = d3.axisRight(y1).ticks(5);
+
+    this.valueline = d3.line()
+      .x(function(d) {
+        return this.x(d.date);
+      })
+      .y(function(d) {
+        return this.y0(d.close);
+      });
+
+      this.setDiagramSize();
+
+      this.createDiagram();
+
+
+  }
+
+  createDiagram(){
+    d3.select("#vis_linediagram").append("path")
+      .attr("d", this.valueline(this.data))
+    d3.select("#vis_linediagram")
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(this.xAxis);
+    d3.select("#vis_linediagram")
+      .append("g")
+      .attr("class", "y axis")
+      .style("fill", "steelblue")
+      .call(this.yAxisLeft);
+  }
+
+  setDiagramSize(){
+    this.svg = document.getElementById("vis_linediagram");
+
+    // Get the bounds of the SVG content
+    let bbox = this.svg.getBBox();
+    // Update the width and height using the size of the contents
+    this.sizeData = [width, height];
+
+
+
+    this.svg.setAttribute("width", this.sizeData[0]);
+    this.svg.setAttribute("height", this.sizeData[1]);
+
+    if (visType == combinedVisType) this.changeZoom(false, 0.5);
   }
 
   /**
@@ -35,21 +101,21 @@ class LineDiagram {
    * @param  {Array} json The array of json data.
    * @return {Array}       The filtered array of nodes.
    */
-   filterData(json) {
+  filterData(json) {
     // Make a clone of the array
     let data = JSON.parse(JSON.stringify(json));
 
     // TODO: insert code to filter data.
 
-    return data; 
-   }
+    return data;
+  }
 }
 
 /**
-* Create an line diagram visualization from an array.
-* @param {Array}  data   JSON array with the data to visualize.
-* @param {Array}  format The visualization format.
-*/
+ * Create an line diagram visualization from an array.
+ * @param {Array}  data   JSON array with the data to visualize.
+ * @param {Array}  format The visualization format.
+ */
 function createLineDiagram(data, format) {
   lineDiagram = new LineDiagram(data, format);
 }
