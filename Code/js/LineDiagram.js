8b7264ca0421ff9871f0608faf18fb2005bd91b4
heleenvdongen <h.e.j.v.dongen@student.tue.nl>
2021-06-09 16:57:17 +0200

    Addition to axis

--- Code/js/LineDiagram.js
+++ Code/js/LineDiagram.js
@@ -16,27 +16,30 @@ class LineDiagram {
    */
   constructor(json, format) {
 
-    this.parseTime = d3.timeParse("%d-%b-%y");
+    this.parseTime = d3.timeParse("%Y-%m-%d");
+    this.data = json;
 
     this.jsonData = JSON.parse(JSON.stringify(json));
-    this.data = json;
-    this.format = format;
 
-    // TODO: remove this after debugging
     console.log(this.data);
+    this.format = format;
 
     this.margin = {top: 20, right: 40, bottom: 30, left: 50};
     this.width = 960 - this.margin.left - this.margin.right;
     this.height = 500 - this.margin.top - this.margin.bottom;
 
 
+    // TODO: remove this after debugging
+
     this.mapJSONData();
   }
+
 
   /**
    * Parse JSON and map data.
    */
   mapJSONData() {
+
     this.dates = []
     this.counts = []
 
@@ -44,86 +47,96 @@ class LineDiagram {
     for (let row of Object.keys(this.data.links)) {
       this.item = this.data.links[row];
       this.item.date = row;
+      this.item.date = this.parseTime(this.item.date);
       this.dates.push(this.item.date);
       this.counts.push(this.item.count);
-      for (let attribute of Object.keys(this.item)) {
 
+      for (let attribute of Object.keys(this.item)) {
         if (attribute != "date") {
           this.item[attribute] = Number(this.item[attribute]);
         }
       }
     }
-
-    this.dates2 = [];
-    for (let row of Object.keys(this.item.date)){
-      this.dates2.push(this.parseTime(Object.keys(this.item)))
+    this.getdata = function() {
+      this.dataset = [];
+      for (let i = 0; i < 20; i++) {
+        const datecount = {"date": this.dates[i], "counts": this.counts[i]};
+        this.dataset.push(datecount);
+      }
+      console.log(this.dataset);
+      return this.dataset
     }
 
-    console.log(this.dates2);
+    this.arrdata = this.getdata();
 
-    this.x = d3.scaleTime()
-      .domain(d3.extent(this.dates, function(d) { return d.date}))
+    //scale X axis
+
+    var domain = d3.extent(this.dates);
+
+    this.scale_x = d3.scaleTime()
+      .domain(domain)
       .range([0,this.width]);
-    this.y0 = d3.scaleLinear().range([this.height, 0]);
-    //var y1 = d3.scaleLinear().range([height, 0]);
 
+    // Scale Y0 axis
+    this.y0 = d3.scaleLinear()
+      .range([this.height, 0])
+      .domain([0, 200]);
 
-    let valueline = d3.line()
-        //.x(function(d) { return x(d.this.links.date); })
-        //.y(function(d) { return y0(d.this.links.count); });
+    // Scale Y1 axis
+    this.y1 = d3.scaleLinear()
+      .range([this.height, 0])
+      .domain([-1,1]);
 
-
+    // Set diagram size
     this.setDiagramSize();
 
-    this.createDiagram(valueline);
-
-
+    //plot diagram
+    this.createDiagram();
   }
 
-  createDiagram(valueline){
-    // Scale the range of the data
-    this.scale_x = d3.scaleTime()
-      .domain([new Date("1998-12-11 00:00:00"), new Date("2002-06-20 00:00:00")])
-      .range([0, width - this.margin.right]);
+  createDiagram(){
 
-    this.y0.domain([0, 200]); //might change later to iterable scale
+    this.arrdata.forEach(function(d){
+      d.count = +d.count;
+      d.new = +this.arrdata.counts;
+    });
+    console.log(this.arrdata);
 
+    //Define first line (count and date)
+    var valueline = d3.line()
+      .x(function(d) { return scale_x(d.x); })
+      .y(function(d) { return y0(d.y); });
 
+      this.svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(this.data));
 
+      // Add the X axis
+      this.svg.append("g")
+          .attr("transform", "translate(0," + this.height + ")")
+          .call(d3.axisBottom(this.scale_x));
 
-    // Add the valueline path.
-    this.svg.append("path")
-      .data([this.counts])
-      .attr("class", "line")
-      .attr("d", valueline);
+        // Add the Y0 Axis
+      this.svg.append("g")
+          .attr("class", "axisSteelBlue")
+          .call(d3.axisLeft(this.y0));
 
-
-
-    // Add the X Axis
-
-
-
-    this.svg.append("g")
-      .attr("transform", "translate(0," + this.height + ")")
-      .call(d3.axisBottom(this.scale_x));
-
-
-    // Add the Y0 Axis
-    this.svg.append("g")
-      .attr("class", "axisSteelBlue")
-      .call(d3.axisLeft(this.y0));
-
-
-
+        // Add the Y1 axis
+      this.svg.append("g")
+        .attr("class", "axisRed")
+        .attr("transform", "translate( " + this.width + ", 0 )")
+        .call(d3.axisRight(this.y1));
   }
 
   setDiagramSize(){
+
     this.svg = d3.select("#vis_linediagram")
       .attr("width", this.width + this.margin.left + this.margin.right)
       .attr("height", this.height + this.margin.top + this.margin.bottom)
       .append("g")
       .attr("transform",
           "translate(" + this.margin.left + "," + this.margin.top + ")");
+
   }
 
 }
