fe9d9364597051b09ff061c660e9853cc5e9a3a2
Martijn van Kekem <martijn@vankekem.com>
2021-06-11 18:16:45 +0200

    Third visualization with interactions

--- Code/js/LineDiagram.js
+++ Code/js/LineDiagram.js
@@ -28,8 +28,7 @@ class LineDiagram {
     this.width = width;
     this.height = 500 - this.margin.top - this.margin.bottom;
 
-
-    // TODO: remove this after debugging
+    this.hoverDistance = 5; // The amount of elements to the left/right of the hovered data point
 
     this.mapJSONData();
   }
@@ -76,25 +75,28 @@ class LineDiagram {
     this.createDiagram(valueline, valueline2);
   }
 
+  /**
+   * Start drawing the visualization
+   * @param {d3.line} valueline  The valueline for the left axis.
+   * @param {d3.line} valueline2 The valueline for the right axis.
+   */
   createDiagram(valueline, valueline2){
     let self = this;
 
+    // Draw the lines for the left axis
     this.svg.append("path")
       .data([this.data.links])
       .attr("class", "line")
       .style("opacity", "0.5")
       .attr("d", valueline);
 
-    // Add the valueline2 path.
+    // Draw the lines for the right axis.
     this.svg.append("path")
       .data([this.data.links])
       .attr("class", "line")
       .style("opacity", "0.5")
       .style("stroke", "red")
-      .attr("d", valueline2)
-      .on("mouseover", d => {
-        // d3.select(d.target).style("fill", "#000");
-      });
+      .attr("d", valueline2);
 
     // Add the X Axis
     this.svg.append("g")
@@ -111,17 +113,133 @@ class LineDiagram {
       .attr("transform", "translate(" + this.width + ", 0)")
       .attr("class", "axisRight")
       .call(d3.axisRight(this.yRight));
+
+    // Draw the hover rectangle for mouse interactions.
+    this.svg.append("rect")
+      .attr("class", "mouseEventRect")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .on("mousemove", (event) => this.mousemove(self, event))
+      .on("mouseleave", (event) => this.mouseleave(self, event))
+      .on("click", (event) => this.mouseclick(self, event));
   }
 
-  setDiagramSize(){
+  /**
+   * Event when the visualization has been clicked.
+   * @param   {LineDiagram} self  The current class context.
+   * @param   {Event}       event The event that was fired.
+   */
+  mouseclick(self, event) {
+    // Only allow mouse click for all vis combined
+    if (visType < combinedVisType) return;
 
+    let [closestIndex, leftIndex, rightIndex] = self.getBoundsByMousePos(self, event);
+
+    const leftElement = self.data.links[leftIndex];
+    const rightElement = self.data.links[rightIndex];
+
+    // TODO: set date filter with left/right bounds.
+  }
+
+  /**
+   * Event when the mouse has moved over the visualization.
+   * @param   {LineDiagram} self  The current class context.
+   * @param   {Event}       event The event that was fired.
+   */
+  mousemove(self, event) {
+    let [closestIndex, leftIndex, rightIndex] = self.getBoundsByMousePos(self, event);
+
+    const leftElement = self.data.links[leftIndex];
+    const rightElement = self.data.links[rightIndex];
+
+    let dateFormat = d3.timeFormat("%Y-%m-%d");
+
+    // Show hover container and put at mouse position
+    let hoverContainer = document.getElementById("hoverContainer_linediagram");
+    hoverContainer.style.display = "block";
+
+    let xOffset = (event.x + hoverContainer.offsetWidth > width) ? (-hoverContainer.offsetWidth - 20) : 20;
+    let yOffset = (event.y + hoverContainer.offsetHeight > height) ? (-hoverContainer.offsetHeight - 20) : 20;
+    hoverContainer.style.left = event.x + xOffset + "px";
+    hoverContainer.style.top = event.y + yOffset + "px";
+
+    // Fill hover container with data
+    document.getElementById("hover_linediagram_closest").innerHTML = dateFormat(self.data.links[closestIndex].date);
+    document.getElementById("hover_linediagram_left").innerHTML = dateFormat(self.data.links[leftIndex].date);
+    document.getElementById("hover_linediagram_right").innerHTML = dateFormat(self.data.links[rightIndex].date);
+    
+    let linkList = document.getElementById("linklist_linediagram");
+    linkList.innerHTML = "";
+    for (let attribute of Object.keys(this.data.links[closestIndex])) {
+      if (attribute == "date") continue;
+
+      let newItem = document.createElement("li");
+      newItem.innerHTML = attribute + ": " + this.data.links[closestIndex][attribute];
+      linkList.appendChild(newItem);
+    }
+
+    // Remove previous drawn lines
+    this.svg.selectAll("line.hoverLineLeft").remove();
+    this.svg.selectAll("line.hoverLineRight").remove();
+
+    // Draw left line
+    this.svg.append("line")
+      .attr("class", "hoverLineLeft")
+      .style("stroke", "black")
+      .attr("x1", self.x(leftElement.date))
+      .attr("y1", 0)
+      .attr("x2", self.x(leftElement.date))
+      .attr("y2", this.height);
+
+    // Draw right line
+    this.svg.append("line")
+    .attr("class", "hoverLineRight")
+      .style("stroke", "black")
+      .attr("x1", self.x(rightElement.date))
+      .attr("y1", 0)
+      .attr("x2", self.x(rightElement.date))
+      .attr("y2", this.height);
+  }
+
+  /**
+   * Event when the mouse has left the visualization.
+   * @param {Event} event The event that was fired.
+   */
+  mouseleave(event) {
+    // Remove left/right lines
+    this.svg.selectAll("line.hoverLineLeft").remove();
+    this.svg.selectAll("line.hoverLineRight").remove();
+  }
+
+  /**
+   * Get the closest index + left and right bounds from the mouse position.
+   * @param   {LineDiagram} self  The current class context.
+   * @param   {Event}       event The event that was fired.
+   * @returns {Array}             The index closest to the mouse, plus the left and right bounds' index. 
+   */
+  getBoundsByMousePos(self, event) {
+    const mousePos = d3.pointer(event);
+    const hoveredDate = self.x.invert(mousePos[0]);
+
+    const getDistanceFromHoveredDate = (d) => Math.abs(d.date - hoveredDate);
+    const closestIndex = d3.scan(self.data.links, (a, b) => getDistanceFromHoveredDate(a) - getDistanceFromHoveredDate(b));
+    
+    const leftIndex = (closestIndex - self.hoverDistance >= 0) ? (closestIndex - self.hoverDistance) : 0;
+    const rightIndex = (closestIndex + self.hoverDistance < self.data.links.length) ? (closestIndex + self.hoverDistance) : (self.data.links.length - 1);
+
+    return [closestIndex, leftIndex, rightIndex];
+  }
+
+  /**
+   * Set the size of the visualization
+   */
+  setDiagramSize(){
     this.svg = d3.select("#vis_linediagram")
       .attr("width", this.width + this.margin.left + this.margin.right)
       .attr("height", this.height + this.margin.top + this.margin.bottom)
       .append("g")
       .attr("transform",
           "translate(" + this.margin.left + "," + this.margin.top + ")");
-
   }
 
 }
